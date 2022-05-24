0f704b27ac2f4e332d107072b17ceae9a60f0802
James <jamescoldwell73@gmail.com>
2022-05-24 12:04:06 +0930

    James Sql coming along

--- SQL.js
+++ SQL.js
@@ -11,10 +11,40 @@ var SQL = await initSqlJs({
   });
 
 
-// either make a new DB or load one
+// do a check to see if i can load the prevDB file. if i cant then it doesnt exist and it is the first time so i should create it
+/* there code
+const xhr = new XMLHttpRequest();
+// For example: https://github.com/lerocha/chinook-database/raw/master/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite
+xhr.open('GET', '/path/to/database.sqlite', true);
+xhr.responseType = 'arraybuffer';
+
+xhr.onload = e => {
+  const uInt8Array = new Uint8Array(xhr.response);
+  const db = new SQL.Database(uInt8Array);
+  const contents = db.exec("SELECT * FROM my_table");
+  // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
+};
+xhr.send();
+*/
 prevDB = false;
+
+
+// either make a new DB or load one
 if (prevDB == false) {
   const db = new SQL.Database();
+
+  // if it is new then i will also need to add all of the tables for the first time
+  let sqlstring = "CREATE TABLE Users_Subjects ( \
+                  Subject_Area TEXT, \
+                  Term TEXT, \
+                  Subject_ID INTEGER, \
+                  Course_title TEXT, \
+                  Timestamp INTEGER, \
+                  User_ID INTEGER \
+                  Scrape_Timestamps, \
+                  PRIMARY KEY (Subject_Area, Term, Subject_ID, Course_title), \
+                  FOREIGN KEY (User_ID), \
+                  FOREIGN KEY (Scrape_Timestamps)"
 } 
 else {
   // prevDBData is an Unit8Array that represents an SQLite DB file
@@ -23,5 +53,10 @@ else {
 
 
 // to add to the database, write the command you want as a string then db.run(string) it
-
+/* There example
+let sqlstr = "CREATE TABLE hello (a int, b char); \
+INSERT INTO hello VALUES (0, 'hello'); \
+INSERT INTO hello VALUES (1, 'world');";
+db.run(sqlstr);
+*/
 
