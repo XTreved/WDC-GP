9310ae07d9eea02aaff527ce6fbc391d1f0c2df8
XTreved <lukeynewy@gmail.com>
2022-05-31 22:28:38 +0930

    WDC: Scraper page format done, still need styling

--- server/public/javascripts/page.js
+++ server/public/javascripts/page.js
@@ -1,10 +1,16 @@
-
+const TRACKED_SUBJECTS = [
+    { title:'ADDS',         day:'Monday'},
+    { title:'SPC',          day:'Tuesday'},
+    { title:'WDC',          day:'Wednesday'}
+];
 
 const SAMPLE_SUBJECTS = [
-    { title:'ADDS',         day:'Monday'    },
-    { title:'SPC',          day:'Tuesday'   },
-    { title:'WDC',          day:'Wednesday' }
-];
+    { title: 'Object Oriented Programming',         id: '1102', area: 'COMP SCI', availability: 'Semester 1'},
+    { title: 'Algorithm Design & Data Structures',  id: '2103', area: 'COMP SCI', availability: 'Semester 2'},
+    { title: 'Hot Topics in IoT Security',          id: '4106', area: 'COMP SCI', availability: 'Trimester 3'},
+    { title: 'Biology I: Human Perspectives',       id: '1201', area: 'BIOLOGY',  availability: 'Semester 2'},
+    { title: 'Postgraduate Professions Internship', id: '7500', area: 'PROF',     availability: 'Trimester 1'}
+]
 
 
 var vueinst = new Vue({
@@ -12,9 +18,14 @@ var vueinst = new Vue({
     data: {
         
         /* Show or hide each section */
-        selected: "login", // login, home, calendar, scraper
+        selected: "scraper", // login, signup, home, calendar, scraper
 
         /* Home page testing */
-        subjects: SAMPLE_SUBJECTS
+        tracked_subjects: TRACKED_SUBJECTS,
+
+        /* Scraper page testing */
+        sample_subjects: SAMPLE_SUBJECTS
+        
+        
     }
 });
