832249967978b2b8ea7c5cfcc0a524d241968627
XTreved <lukeynewy@gmail.com>
2022-06-01 00:57:08 +0930

    WDC: Commenting my codes

--- server/public/javascripts/page.js
+++ server/public/javascripts/page.js
@@ -1,9 +1,12 @@
+// This was used to test the overview page
 const TRACKED_SUBJECTS = [
     { title:'ADDS',         day:'Monday'},
     { title:'SPC',          day:'Tuesday'},
     { title:'WDC',          day:'Wednesday'}
 ];
 
+
+// This was used to test the scraper page
 const SAMPLE_SUBJECTS = [
     { title: 'Object Oriented Programming',         id: '1102', area: 'COMP SCI', availability: 'Semester 1'},
     { title: 'Algorithm Design & Data Structures',  id: '2103', area: 'COMP SCI', availability: 'Semester 2'},
@@ -18,14 +21,12 @@ var vueinst = new Vue({
     data: {
         
         /* Show or hide each section */
-        selected: "scraper", // login, signup, home, calendar, scraper
+        selected: "login", // login, signup, home, calendar, scraper
 
         /* Home page testing */
         tracked_subjects: TRACKED_SUBJECTS,
 
         /* Scraper page testing */
         sample_subjects: SAMPLE_SUBJECTS
-        
-        
     }
 });
